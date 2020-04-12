a17977d098e50fd300a4c5d68b7aaa0596c0904c
Toshiaki Maki <tmaki@pivotal.io>
2020-04-13 02:04:55 +0900

    Add Download Button

--- azure-dns-mgmt-ui/src/index.js
+++ azure-dns-mgmt-ui/src/index.js
@@ -6,6 +6,13 @@ const initialize = () => {
     };
     reloadButton.addEventListener('click', reload);
     reload().then();
+    dnsZones.addEventListener('click', e => {
+        const elm = e.target;
+        if (elm.type === 'button' && elm.dataset && elm.dataset.name) {
+            const name = elm.dataset.name;
+            downloadCertificates(name);
+        }
+    });
 };
 
 const loadDnsZones = () => fetch('/dns_zones')
@@ -14,7 +21,18 @@ const loadDnsZones = () => fetch('/dns_zones')
 <td>${zone.name}</td>
 <td>${zone.createdBy}</td>
 <td>${zone.createdAt}</td>
-<td>${zone.certificate}</td>
+<td>${zone.certificate && downloadCertificatesButton(zone.name)}</td>
 </tr>`).join(''));
+
+
+const downloadCertificates = (name) => {
+    const a = document.createElement('a');
+    a.href = `/certificates/${name}`;
+    a.click();
+};
+
+const downloadCertificatesButton = (name) => {
+    return `<span><button type="button" class="pui-btn pui-btn--primary" data-name="${name}">Download</button></span>`;
+};
 
 document.addEventListener('DOMContentLoaded', initialize);
